1dcda703c73fb6be7f740dc3d9f00a265c0d0096
mehdiparastar <parastar.mehdi@gmail.com>
2021-03-31 14:23:37 +0430

    65

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from 'esbuild-wasm'
 import { useEffect, useState, useRef } from 'react'
 import ReactDom from 'react-dom'
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 
 const App = () => {
     const ref = useRef<any>()
@@ -18,11 +19,13 @@ const App = () => {
         if (!ref.current) {
             return
         }
-        const result = await ref.current.transform(input, {
-            loader: 'jsx',
-            target: 'es2015'
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()]
         })
-        setCode(result.code)
+        setCode(result.outputFiles[0].text)
     }
 
     useEffect(() => {
