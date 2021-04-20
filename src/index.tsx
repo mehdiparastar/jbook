09c4064b79ac273d4e60137b9472e77c6649c9f4
mehdiparastar <parastar.mehdi@gmail.com>
2021-04-20 11:29:16 +0430

    70

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,52 @@
-import * as esbuild from 'esbuild-wasm'
-import { useEffect, useState, useRef } from 'react'
-import ReactDom from 'react-dom'
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
-    const ref = useRef<any>()
-    const [input, setInput] = useState('')
-    const [code, setCode] = useState('')
-
+    const ref = useRef<any>();
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
 
     const startService = async () => {
         ref.current = await esbuild.startService({
             worker: true,
-            wasmURL: '/esbuild.wasm'
-        })
-    }
+            wasmURL: '/esbuild.wasm',
+        });
+    };
+    useEffect(() => {
+        startService();
+    }, []);
+
     const onClick = async () => {
         if (!ref.current) {
-            return
+            return;
         }
+
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
             write: false,
-            plugins: [unpkgPathPlugin()]
-        })
-        setCode(result.outputFiles[0].text)
-    }
+            plugins: [unpkgPathPlugin()],
+        });
 
-    useEffect(() => {
-        startService()
-    }, [])
+        // console.log(result);
 
-    return <div>
-        <textarea
-            value={input}
-            onChange={e => setInput(e.target.value)}
-        ></textarea>
+        setCode(result.outputFiles[0].text);
+    };
+
+    return (
         <div>
-            <button onClick={onClick}>submit</button>
+            <textarea
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            ></textarea>
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
         </div>
-        <pre>{code}</pre>
-    </div>
-}
+    );
+};
 
-ReactDom.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'));
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,12 +1,26 @@
 import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
 
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        return { path: args.path, namespace: 'a' };
+        console.log('onResolve', args);
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        }
+
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`
+        }
+        // else if (args.path === 'tiny-test-pkg') {
+        //   return {
+        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+        //     namespace: 'a',
+        //   };
+        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -16,11 +30,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'tiny-test-pkg';
+              const message = require('tiny-test-pkg');
               console.log(message);
             `,
           };
         }
+
+        const { data } = await axios.get(args.path);
+        return {
+          loader: 'jsx',
+          contents: data,
+        };
       });
     },
   };
